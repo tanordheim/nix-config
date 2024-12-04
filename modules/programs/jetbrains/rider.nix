d2e6ac6578b76c13df35c1ec37dc7d75c7c58b56
trond <trond@nordheim.io>
2024-12-04 18:25:42 +0100

    set rider vmoptions

--- modules/programs/jetbrains/rider.nix
+++ modules/programs/jetbrains/rider.nix
@@ -1,6 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, isDarwin, ... }:
+let
+  vmOptionsFile =
+    if isDarwin then
+      "Library/Application Support/JetBrains/Rider${pkgs.jetbrains.rider.version}/rider.vmoptions"
+    else
+      ".config/JetBrains/Rider${pkgs.jetbrains.rider.version}/rider.vmoptions";
+
+in
 {
   my.user.home.packages = [
     pkgs.jetbrains.rider
   ];
+
+  my.user.home.file = {
+    "${vmOptionsFile}" = {
+      text = ''
+      -Xms1g
+      -Xmx2g
+      '';
+    };
+  };
 }
