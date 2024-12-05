83996f769a49c3ec52155b41f0fbb145612be684
trond <trond@nordheim.io>
2024-12-05 22:07:36 +0100

    ensure macos wallpaper is set on all desktops

--- modules/desktop/wallpaper/_darwin.nix
+++ modules/desktop/wallpaper/_darwin.nix
@@ -4,15 +4,10 @@ let
 
 in
 {
-  environment.systemPackages = with pkgs; [
-    m-cli
-  ];
-
   my.user = { lib, config, ... }: {
     home.activation = {
       setDarwinWallpaper = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        # need to temporarily set path for osascript to be callable by m-cli
-        PATH="/usr/bin:$PATH" ${pkgs.m-cli}/bin/m wallpaper "${config.lib.file.mkOutOfStoreSymlink wallpaperPath}"
+        /usr/bin/osascript -e 'tell application "System Events" to tell every desktop to set picture to "${config.lib.file.mkOutOfStoreSymlink wallpaperPath}" as POSIX file'
       '';
     };
   };
