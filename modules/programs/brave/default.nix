bd72f4c3af0619db3be6cfae11f902e460843810
trond <trond@nordheim.io>
2024-12-17 23:36:27 +0100

    set brave system policy

--- modules/programs/brave/default.nix
+++ modules/programs/brave/default.nix
@@ -6,4 +6,16 @@
 }:
 {
   imports = [ ] ++ lib.optional isDarwin ./_darwin.nix ++ lib.optional isLinux ./_nixos.nix;
+
+  # see brave://policy/
+  environment.etc."brave/policies/managed/default.json".text = builtins.toJSON {
+    BraveAIChatEnabled = false;
+    BraveRewardsDisabled = true;
+    BraveVPNDisabled = true;
+    BraveWalletDisabled = true;
+    DefaultSearchProviderEnabled = true;
+    DefaultSearchProviderName = "Google";
+    DefaultSearchProviderSearchURL = "https://www.google.com/search?q={searchTerms}";
+    DefaultSearchProviderSuggestURL = "https://www.google.com/complete/search?output=chrome&q={searchTerms}";
+  };
 }
