4272785cab3ff51c7b55277c26729d8524f68488
trond <trond@nordheim.io>
2024-12-17 22:42:09 +0100

    remove unused dunst config

--- modules/desktop/notifications/_nixos.nix
+++ modules/desktop/notifications/_nixos.nix
@@ -13,41 +13,4 @@
       };
     };
   };
-
-  # [global]
-  # monitor = 0
-  # width = 300
-  # offset = 10x10
-  # font = JetBrains Mono, Monospace 10
-  # frame_color = "#89b4fa"
-  # separator_color= frame
-  #
-  # [urgency_low]
-  # background = "#1e1e2e"
-  # foreground = "#cdd6f4"
-  #
-  # [urgency_normal]
-  # background = "#1e1e2e"
-  # foreground = "#cdd6f4"
-  #
-  # [urgency_critical]
-  # background = "#1e1e2e"
-  # foreground = "#cdd6f4"
-  # frame_color = "#fab387"
-
-  # my.user.programs.rofi = {
-  #   enable = true;
-  #   package = pkgs.rofi-wayland;
-  #   terminal = "${pkgs.alacritty}/bin/alacritty";
-  #   extraConfig = {
-  #     modi = "run,drun,window";
-  #     drun-display-format = "{icon} {name}";
-  #     hide-scrollbar = true;
-  #     display-drun = "   Apps ";
-  #     display-run = "   Run ";
-  #     display-window = " 󰕰  Window";
-  #     display-Network = " 󰤨  Network";
-  #     sidebar-mode = true;
-  #   };
-  # };
 }
