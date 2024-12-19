b07b7a07e7ffd0867a56d149f55945d6fe20e6ff
trond <trond@nordheim.io>
2024-12-19 09:39:41 +0100

    set datagrip opts, set GOPATH

--- modules/programs/go.nix
+++ modules/programs/go.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   environment.systemPackages = with pkgs; [
     go
     golangci-lint
     gotools
   ];
+
+  environment.sessionVariables = {
+    GOPATH = "${config.my.osUser.home}/.local/share/go";
+  };
 }
--- modules/programs/jetbrains/datagrip.nix
+++ modules/programs/jetbrains/datagrip.nix
@@ -1,6 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, isDarwin, ... }:
+let
+  plugins = import ./plugins.nix { inherit pkgs; };
+
+  vmOptionsFile =
+    if isDarwin then
+      "Library/Application Support/JetBrains/DataGrip${pkgs.jetbrains.datagrip.version}/rider.vmoptions"
+    else
+      ".config/JetBrains/DataGrip${pkgs.jetbrains.datagrip.version}/datagrip64.vmoptions";
+
+  vmOptionsContent =
+    if isDarwin then
+      ''''
+    else
+      ''
+        -Dawt.toolkit.name=WLToolkit
+      '';
+in
 {
   my.user.home.packages = [
-    pkgs.jetbrains.datagrip
+    (pkgs.jetbrains.plugins.addPlugins pkgs.jetbrains.datagrip plugins)
   ];
+
+  my.user.home.file = {
+    "${vmOptionsFile}".text = vmOptionsContent;
+  };
 }
