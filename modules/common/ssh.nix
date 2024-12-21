07ef0d104cc2cab0bd7f87edd5bd1322b2e2c905
trond <trond@nordheim.io>
2024-12-21 18:03:42 +0100

    set github.com ssh host, update private flakes

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -1,4 +1,11 @@
 { config, ... }:
 {
-  home-manager.users.${config.username}.programs.ssh.enable = true;
+  home-manager.users.${config.username}.programs.ssh = {
+    enable = true;
+    matchBlocks."github.com" = {
+      host = "github.com";
+      hostname = "ssh.github.com";
+      port = 443;
+    };
+  };
 }
