99e8b2ab6263795e6dea79ae4374d16a522b74ab
trond <trond@nordheim.io>
2024-12-23 11:14:29 +0100

    make hyprland animations more snappy

--- modules/linux/hyprland.nix
+++ modules/linux/hyprland.nix
@@ -70,13 +70,11 @@
 
         animations = {
           enabled = true;
-          bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
           animation = [
-            "windows, 1, 7, myBezier"
-            "windowsOut, 1, 7, default, popin, 80%"
+            "windows, 1, 2, default, popin"
+            "windowsOut, 1, 2, default, popin"
             "border, 1, 10, default"
-            "borderangle, 1, 8, default"
-            "fade, 1, 7, default"
+            "fade, 1, 5, default"
             "workspaces, 1, 2, default"
           ];
         };
