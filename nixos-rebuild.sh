536d80e2623e398afd1da7d13b3261d9e3612fcc
trond <trond@nordheim.io>
2025-01-06 16:46:39 +0100

    313 current  2025-01-06 16:46:21  25.05.20241213.71a6392  6.12.4-asahi                          *

--- nixos-rebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-sudo nixos-rebuild switch --impure --flake .
-current=$(nixos-rebuild list-generations | grep current)
-git commit -am "$current"
-notify-send -e "NixOS Rebuilt OK" --icon=software-update-available
