e2d3b6687aaa0ae3d3bb1aebaeb7fc37df30ccc6
trond <trond@nordheim.io>
2025-01-08 12:35:06 +0100

    351 current  2025-01-08 12:34:58  25.05.20241223.b74a56e  6.12.4-asahi                          *

--- modules/common/neovim/plugins/snacks.nix
+++ modules/common/neovim/plugins/snacks.nix
@@ -35,10 +35,28 @@ in
                 },
             }
 
+            -- show lsp progress
+            vim.api.nvim_create_autocmd("LspProgress", {
+              ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+              callback = function(ev)
+                local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+                vim.notify(vim.lsp.status(), "info", {
+                  id = "lsp_progress",
+                  title = "LSP Progress",
+                  opts = function(notif)
+                    notif.icon = ev.data.params.value.kind == "end" and " "
+                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+                  end,
+                })
+              end,
+            })
+
             vim.keymap.set('n', '<leader>bd', function() Snacks.bufdelete() end, { desc = "[D]elete current [b]uffer" })
             vim.keymap.set('n', '<leader>un', function() Snacks.notifier.hide() end, { desc = "Dismiss all notifications" })
+            vim.keymap.set('n', '<leader>sn', function() Snacks.notifier.show_history() end, { desc = "Show notification history" })
             vim.keymap.set('n', '<leader>rN', function() Snacks.rename.rename_file() end, { desc = "[R]e[n]ame File" })
             vim.keymap.set(''\'', '<C-;>', function() Snacks.terminal() end, { desc = "Toggle terminal" })
+
           '';
       }
     ];
