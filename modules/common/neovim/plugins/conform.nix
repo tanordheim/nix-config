9b6d11401777895b9420206204fae09b6d3fc98e
trond <trond@nordheim.io>
2025-01-08 13:58:10 +0100

    365 current  2025-01-08 13:58:01  25.05.20241223.b74a56e  6.12.4-asahi                          *

--- modules/common/neovim/plugins/conform.nix
+++ modules/common/neovim/plugins/conform.nix
@@ -12,7 +12,7 @@
         config = # lua
           ''
             require('conform').setup {
-              notify_on_error = false,
+              notify_on_error = true,
               format_on_save = function(bufnr)
                 -- Disable "format_on_save lsp_fallback" for languages that don't
                 -- have a well standardized coding style. You can add additional
@@ -44,7 +44,7 @@
               formatters = {
                 csharpier = {
                   command = "${pkgs.dotnet-sdk}/bin/dotnet",
-                  args = { 'tool', 'run', 'dotnet-csharpier', '--write-stdout' },
+                  args = { 'tool', 'run', '--allow-roll-forward', 'dotnet-csharpier', '--write-stdout' },
                 },
                 goimports = {
                   command = "${pkgs.gotools}/bin/goimports"
