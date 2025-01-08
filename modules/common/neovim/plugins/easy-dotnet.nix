4d2ef3ebc4e37eb9bf3766d479d0fe18aada2c17
trond <trond@nordheim.io>
2025-01-08 11:57:22 +0100

    344 current  2025-01-08 11:57:12  25.05.20241223.b74a56e  6.12.4-asahi                          *

--- modules/common/neovim/plugins/easy-dotnet.nix
+++ modules/common/neovim/plugins/easy-dotnet.nix
@@ -46,61 +46,63 @@ in
             -- })
 
             roslyn.setup({
-              exe = 'Microsoft.CodeAnalysis.LanguageServer',
-              args = {
-                "--logLevel=Information", "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path())
-              },
-              on_attach = function() 
-                if client.is_hacked then
-                  return
-                end
-                client.is_hacked = true
-                print("monkeypatching roslyn")
-
-                -- let the runtime know the server can do semanticTokens/full now
-                client.server_capabilities = vim.tbl_deep_extend('force', client.server_capabilities, {
-                  semanticTokensProvider = {
-                    full = true,
-                  },
-                })
-
-                -- monkey patch the request proxy
-                local request_inner = client.request
-                client.request = function(method, params, handler, req_bufnr)
-                  if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
-                    return request_inner(method, params, handler)
+              config = {
+                on_attach = function()
+                  if client.is_hacked then
+                    return
                   end
+                  client.is_hacked = true
+                  print("monkeypatching roslyn")
 
-                  local function find_buf_by_uri(search_uri)
-                    local bufs = vim.api.nvim_list_bufs()
-                    for _, buf in ipairs(bufs) do
-                      local name = vim.api.nvim_buf_get_name(buf)
-                      local uri = 'file://' .. name
-                      if uri == search_uri then
-                        return buf
+                  -- let the runtime know the server can do semanticTokens/full now
+                  client.server_capabilities = vim.tbl_deep_extend('force', client.server_capabilities, {
+                    semanticTokensProvider = {
+                      full = true,
+                    },
+                  })
+
+                  -- monkey patch the request proxy
+                  local request_inner = client.request
+                  client.request = function(method, params, handler, req_bufnr)
+                    if method ~= vim.lsp.protocol.Methods.textDocument_semanticTokens_full then
+                      return request_inner(method, params, handler)
+                    end
+
+                    local function find_buf_by_uri(search_uri)
+                      local bufs = vim.api.nvim_list_bufs()
+                      for _, buf in ipairs(bufs) do
+                        local name = vim.api.nvim_buf_get_name(buf)
+                        local uri = 'file://' .. name
+                        if uri == search_uri then
+                          return buf
+                        end
                       end
                     end
+
+                    local target_bufnr = find_buf_by_uri(params.textDocument.uri)
+                    local line_count = vim.api.nvim_buf_line_count(target_bufnr)
+                    local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
+
+                    return request_inner('textDocument/semanticTokens/range', {
+                      textDocument = params.textDocument,
+                      range = {
+                        ['start'] = {
+                          line = 0,
+                          character = 0,
+                        },
+                        ['end'] = {
+                          line = line_count - 1,
+                          character = string.len(last_line) - 1,
+                        },
+                      },
+                    }, handler, req_bufnr)
                   end
-
-                  local target_bufnr = find_buf_by_uri(params.textDocument.uri)
-                  local line_count = vim.api.nvim_buf_line_count(target_bufnr)
-                  local last_line = vim.api.nvim_buf_get_lines(target_bufnr, line_count - 1, line_count, true)[1]
-
-                  return request_inner('textDocument/semanticTokens/range', {
-                    textDocument = params.textDocument,
-                    range = {
-                      ['start'] = {
-                        line = 0,
-                        character = 0,
-                      },
-                      ['end'] = {
-                        line = line_count - 1,
-                        character = string.len(last_line) - 1,
-                      },
-                    },
-                  }, handler, req_bufnr)
-                end
-              end,
+                end,
+              }
+              exe = 'Microsoft.CodeAnalysis.LanguageServer',
+              args = {
+                "--logLevel=Debug", "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path())
+              },
               capabilities = capabilities
               -- config = {
               --   settings = {
