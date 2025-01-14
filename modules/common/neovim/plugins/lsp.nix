121e42f6c939b71f66ac24e5d55434feee43a6e0
trond <trond@nordheim.io>
2025-01-14 15:23:18 +0100

    386 current  2025-01-14 15:23:07  25.05.20250110.130595e  6.12.4-asahi                          *

--- modules/common/neovim/plugins/lsp.nix
+++ modules/common/neovim/plugins/lsp.nix
@@ -99,6 +99,9 @@
                 local client = vim.lsp.get_client_by_id(ev.data.client_id)
                 if client.server_capabilities.inlayHintProvider then
                   vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+                  print("enabling inlay hints provider")
+                else
+                  print("no inlay hints provider, not enabling")
                 end
 
                 -- Buffer local mappings.
