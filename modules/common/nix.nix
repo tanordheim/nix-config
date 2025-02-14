8232570dcab3d45121d7e6e6724a40d1cce29414
trond <trond@nordheim.io>
2025-02-14 10:07:47 +0100

    531 current  2025-02-14 10:07:42  25.05.20250213.2ff53fe  6.12.12-asahi                          *

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -33,31 +33,5 @@
     (prev: final: {
       stable = import nixpkgs-stable { inherit (prev) system; };
     })
-    (_final: prev: {
-      vimPlugins = prev.vimPlugins // {
-        snacks-vim =
-          let
-            version = "v2.20.0";
-          in
-          (prev.vimUtils.buildVimPlugin {
-            pname = "snacks.nvim";
-            inherit version;
-
-            src = prev.fetchFromGitHub {
-              owner = "folke";
-              repo = "snacks.nvim";
-              rev = "76a5dcfb318d623022dada44c66453d9cb9a6eaa";
-              hash = "sha256-DLbXRDBKGxe3JcgrqNp4FPJq/yKZZcGdOR6I9b3+YCo=";
-            };
-
-            doCheck = false;
-
-            meta = {
-              description = "🍿 A collection of QoL plugins for Neovim";
-              homepage = "https://github.com/folke/snacks.nvim";
-            };
-          });
-      };
-    })
   ];
 }
