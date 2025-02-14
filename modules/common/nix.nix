3e9f1e9b1d155df8c83747bfc9ebe004f2be3648
trond <trond@nordheim.io>
2025-02-14 09:51:17 +0100

    527 current  2025-02-14 09:51:12  25.05.20250213.2ff53fe  6.12.12-asahi                          *

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -31,7 +31,33 @@
   };
   nixpkgs.overlays = [
     (prev: final: {
+      # vimPlugins = prev.vimPlugins // {
+      #   snacks-vim =
+      #     let
+      #       version = "2025-02-12";
+      #     in
+      #     (prev.vimUtils.buildVimPlugin {
+      #       pname = "snacks.nvim";
+      #       inherit version;
+      #
+      #       src = prev.fetchFromGitHub {
+      #         owner = "folke";
+      #         repo = "snacks.nvim";
+      #         rev = "1491b543ef1d8a0eb29a6ebc35db4fb808dcb47f";
+      #         hash = "sha256-DLbXRDBKGxe3JcgrqNp4FPJq/yKZZcGdOR6I9b3+YCo=";
+      #       };
+      #
+      #       doCheck = false;
+      #
+      #       meta = {
+      #         description = "🍿 A collection of QoL plugins for Neovim";
+      #         homepage = "https://github.com/folke/snacks.nvim";
+      #       };
+      #     });
+      # };
       stable = import nixpkgs-stable { inherit (prev) system; };
+    })
+    (prev: final: {
     })
   ];
 }
