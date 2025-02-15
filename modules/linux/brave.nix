2731cc5a2060031d08ff823a343edec76f7a73c3
trond <trond@nordheim.io>
2025-02-15 09:41:39 +0100

    540 current  2025-02-15 09:41:34  25.05.20250213.2ff53fe  6.12.12-asahi                          *

--- modules/linux/brave.nix
+++ modules/linux/brave.nix
@@ -5,7 +5,7 @@
   ];
 
   home-manager.users.${config.username}.xdg.desktopEntries.brave-browser = {
-    exec = "brave --ozone-platform=wayland --js-flags=--no-decommit-pooled-pages %u";
+    exec = "brave --ozone-platform=wayland --js-flags=--no-decommit-pooled-pages --new-window %u";
     icon = "brave";
     name = "Brave";
     terminal = false;
