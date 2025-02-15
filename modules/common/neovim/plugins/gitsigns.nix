8e3ae865edbc0b120b7dd2847680311ebd26e145
trond <trond@nordheim.io>
2025-02-15 09:56:29 +0100

    544 current  2025-02-15 09:56:25  25.05.20250213.2ff53fe  6.12.12-asahi                          *

--- modules/common/neovim/plugins/gitsigns.nix
+++ modules/common/neovim/plugins/gitsigns.nix
@@ -3,7 +3,6 @@
   home-manager.users.${config.username}.programs.nixvim.plugins.gitsigns = {
     enable = true;
     settings = {
-      signcolumn = false; # handled by snacks
       current_line_blame = true;
       numhl = true;
     };
