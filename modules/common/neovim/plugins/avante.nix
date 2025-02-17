2621bd6f7cc4dc6f92bed8fb94a8c653428ce18b
trond <trond@nordheim.io>
2025-02-17 22:23:34 +0100

    577 current  2025-02-17 22:23:29  25.05.20250216.d74a233  6.12.12-asahi                          *

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -4,6 +4,14 @@
     enable = true;
     settings = {
       provider = "openai";
+      openai = {
+        endpoint = "https://api.openai.com/v1";
+        model = "gpt-4o";
+        timeout = 30000;
+        temperature = 0;
+        max_tokens = 4096;
+        reasoning_effort = "high";
+      };
     };
   };
 }
