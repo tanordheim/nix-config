daa1b8ef00c36bde663c51ca9f460287310a3032
trond <trond@nordheim.io>
2025-02-18 13:53:21 +0100

    add grpc-nvim

---
+++ modules/common/neovim/plugins/grpc-nvim.nix
@@ -0,0 +1,21 @@
+{ pkgs, config, ... }:
+let
+  grpc-nvim-plugin = pkgs.vimUtils.buildVimPlugin {
+    pname = "grpc-nvim";
+    version = "2023-01-22";
+    doCheck = false;
+    src = pkgs.fetchFromGitHub {
+      owner = "hudclark";
+      repo = "grpc-nvim";
+      rev = "cd235398a9922ae412f59ebeabb0b13957be0d39";
+      hash = "";
+    };
+  };
+in
+{
+  home-manager.users.${config.username}.programs.nixvim = {
+    extraPlugins = [
+      grpc-nvim-plugin
+    ];
+  };
+}
