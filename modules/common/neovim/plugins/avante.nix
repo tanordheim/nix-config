f17141887b25f7959542437aac492fbec53794a2
trond <trond@nordheim.io>
2025-02-24 13:00:41 +0100

    609 current  2025-02-24 13:00:37  25.05.20250221.32fb99b  6.12.12-asahi                          *

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -3,14 +3,19 @@
   home-manager.users.${config.username}.programs.nixvim.plugins.avante = {
     enable = true;
     settings = {
-      provider = "openai";
+      provider = "claude";
+      claude = {
+        endpoint = "https://api.anthropic.com";
+        model = "claude-3-5-sonnet-20241022";
+        temperature = 0;
+        max_tokens = 4096;
+      };
       openai = {
         endpoint = "https://api.openai.com/v1";
         model = "gpt-4o";
         timeout = 30000;
         temperature = 0;
         max_tokens = 4096;
-        # reasoning_effort = "high";
       };
     };
   };
