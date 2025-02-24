834b268ff88b4fd6b2b206318ecad237bd7165ee
trond <trond@nordheim.io>
2025-02-24 13:20:57 +0100

    611 current  2025-02-24 13:20:52  25.05.20250221.32fb99b  6.12.12-asahi                          *

--- modules/common/neovim/plugins/codecompanion-nvim.nix
+++ modules/common/neovim/plugins/codecompanion-nvim.nix
@@ -3,14 +3,16 @@
   home-manager.users.${config.username}.programs.nixvim.plugins.codecompanion = {
     enable = true;
     settings = {
-      chat = {
-        adapter = "anthropic";
-      };
-      inline = {
-        adapter = "anthropic";
-      };
-      agent = {
-        adapter = "anthropic";
+      strategies = {
+        chat = {
+          adapter = "anthropic";
+        };
+        inline = {
+          adapter = "anthropic";
+        };
+        agent = {
+          adapter = "anthropic";
+        };
       };
     };
   };
