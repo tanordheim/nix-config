2f025f7a80fb023e229c98194ee3639501592947
trond <trond@nordheim.io>
2025-02-27 19:35:36 +0100

    632 current  2025-02-27 19:35:30  25.05.20250221.32fb99b  6.12.12-asahi                          *

--- modules/linux/chromium.nix
+++ modules/linux/chromium.nix
@@ -3,6 +3,10 @@
   home-manager.users.${config.username}.programs.chromium = {
     enable = true;
     package = pkgs.chromium;
+    commandLineArgs = [
+      "--oauth2-client-id=77185425430.apps.googleusercontent.com"
+      "--oauth2-client-secret=OTJgUOQcT7lO7GsGZq2G4IlT"
+    ];
     extensions = [
       # 1Password
       "aeblfdkhhhdcdjpifhhbdiojplfjncoa"
