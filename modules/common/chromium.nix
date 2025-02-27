250c7d3976be7db7c218a7e94dddab3352e5419d
trond <trond@nordheim.io>
2025-02-27 19:39:34 +0100

    633 current  2025-02-27 19:39:29  25.05.20250221.32fb99b  6.12.12-asahi                          *

--- modules/common/chromium.nix
+++ modules/common/chromium.nix
@@ -16,4 +16,8 @@ in
   environment.etc."chromium/policies/managed/default.json".text = builtins.toJSON {
     DownloadDirectory = downloadDirectory;
   };
+  home-manager.users.${config.username}.home.sessionVariables = {
+    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
+    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
+  };
 }
--- modules/linux/chromium.nix
+++ modules/linux/chromium.nix
@@ -3,10 +3,6 @@
   home-manager.users.${config.username}.programs.chromium = {
     enable = true;
     package = pkgs.chromium;
-    commandLineArgs = [
-      "--oauth2-client-id=77185425430.apps.googleusercontent.com"
-      "--oauth2-client-secret=OTJgUOQcT7lO7GsGZq2G4IlT"
-    ];
     extensions = [
       # 1Password
       "aeblfdkhhhdcdjpifhhbdiojplfjncoa"
