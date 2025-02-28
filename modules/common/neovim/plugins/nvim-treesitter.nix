614951a061bb2fc37613bf063a7543b7925a4bd1
trond <trond@nordheim.io>
2025-02-28 14:44:10 +0100

    655 current  2025-02-28 14:44:05  25.05.20250226.5135c59  6.12.12-asahi                          *

--- modules/common/neovim/plugins/nvim-treesitter.nix
+++ modules/common/neovim/plugins/nvim-treesitter.nix
@@ -1,33 +1,60 @@
 { pkgs, config, ... }:
 {
-  home-manager.users.${config.username}.programs.nixvim.plugins.treesitter = {
-    enable = true;
-    settings = {
-      highlight.enable = true;
-      indent.enable = true;
-    };
+  home-manager.users.${config.username}.programs.nixvim.plugins = {
+    treesitter = {
+      enable = true;
+      settings = {
+        highlight.enable = true;
+        indent.enable = true;
+      };
 
-    # grammar packages not covered by a language setup under ./languages/
-    grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      bash
-      c
-      css
-      devicetree
-      diff
-      helm
-      http
-      ini
-      javascript
-      just
-      json
-      jsonc
-      typescript
-      markdown
-      markdown_inline
-      query
-      rasi
-      vim
-      vimdoc
-    ];
+      # grammar packages not covered by a language setup under ./languages/
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        bash
+        c
+        css
+        devicetree
+        diff
+        helm
+        http
+        ini
+        javascript
+        just
+        json
+        jsonc
+        typescript
+        markdown
+        markdown_inline
+        query
+        rasi
+        vim
+        vimdoc
+      ];
+    };
+    treesitter-textobjects = {
+      enable = true;
+      select = {
+        enable = true;
+        lookahead = true;
+        keymaps = {
+          "af" = {
+            query = "@function.outer";
+            desc = "Select outer part of function";
+          };
+          "if" = {
+            query = "@function.inner";
+            desc = "Select outer part of function";
+          };
+          "ac" = {
+            query = "@class.outer";
+            desc = "Select outer part of class";
+          };
+          "ic" = {
+            query = "@class.inner";
+            desc = "Select inner part of class";
+          };
+        };
+      };
+    };
   };
 }
