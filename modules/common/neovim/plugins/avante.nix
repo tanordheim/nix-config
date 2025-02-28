3bab8ac6d5c53806c785d0afb002d976d3912d6b
trond <trond@nordheim.io>
2025-02-28 10:07:19 +0100

    645 current  2025-02-28 10:07:13  25.05.20250226.5135c59  6.12.12-asahi                          *

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -11,26 +11,40 @@ let
       hash = "sha256-+ve7Stk47hWKWSynpUlCO/qk9ikisJ5Z9jap9B37PC0=";
     };
   };
+  img-clip-plugin = pkgs.vimUtils.buildVimPlugin {
+    pname = "img-clip.nvim";
+    version = "0.6.0";
+    doCheck = false;
+    src = pkgs.fetchFromGitHub {
+      owner = "HakonHarnes";
+      repo = "img-clip.nvim";
+      rev = "v0.6.0";
+      hash = "sha256-CBZVQ7gTDAONfHrSJEUL4mBjjxj8eeV2KCuZIA/USv8=";
+    };
+  };
 in
 {
-  home-manager.users.${config.username}.programs.nixvim.plugins.avante = {
-    enable = true;
-    package = avante-nvim-plugin;
-    settings = {
-      provider = "claude";
-      claude = {
-        endpoint = "https://api.anthropic.com";
-        model = "claude-3-7-sonnet-20250219";
-        temperature = 0;
-        max_tokens = 4096;
-      };
-      openai = {
-        endpoint = "https://api.openai.com/v1";
-        model = "gpt-4o";
-        timeout = 30000;
-        temperature = 0;
-        max_tokens = 4096;
+  home-manager.users.${config.username}.programs.nixvim = {
+    plugins.avante = {
+      enable = true;
+      package = avante-nvim-plugin;
+      settings = {
+        provider = "claude";
+        claude = {
+          endpoint = "https://api.anthropic.com";
+          model = "claude-3-7-sonnet-20250219";
+          temperature = 0;
+          max_tokens = 4096;
+        };
+        openai = {
+          endpoint = "https://api.openai.com/v1";
+          model = "gpt-4o";
+          timeout = 30000;
+          temperature = 0;
+          max_tokens = 4096;
+        };
       };
     };
+    extraPlugins = [ img-clip-plugin ];
   };
 }
