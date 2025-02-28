0924bbec05857c231b649ca9e243773a008ba6ef
trond <trond@nordheim.io>
2025-02-28 09:01:10 +0100

    636 current  2025-02-28 09:01:04  25.05.20250226.5135c59  6.12.12-asahi                          *

--- modules/linux/chromium.nix
+++ modules/linux/chromium.nix
@@ -1,17 +1,27 @@
 { pkgs, config, ... }:
 {
-  home-manager.users.${config.username}.programs.chromium = {
-    enable = true;
-    package = pkgs.chromium;
-    extensions = [
-      # 1Password
-      "aeblfdkhhhdcdjpifhhbdiojplfjncoa"
+  home-manager.users.${config.username} = {
+    programs.chromium = {
+      enable = true;
+      package = pkgs.chromium;
+      extensions = [
+        # 1Password
+        "aeblfdkhhhdcdjpifhhbdiojplfjncoa"
 
-      # Detrumpify
-      "hfhaalldkgmfbjjehkiddheghljjdjln"
+        # Detrumpify
+        "hfhaalldkgmfbjjehkiddheghljjdjln"
 
-      # Feedly Notifier
-      "egikgfbhipinieabdmcpigejkaomgjgb"
-    ];
+        # Feedly Notifier
+        "egikgfbhipinieabdmcpigejkaomgjgb"
+      ];
+    };
+    xdg.mimeApps.defaultApplications = {
+      "default-web-browser" = [ "chromium-desktop.desktop" ];
+      "text/html" = [ "chromium-desktop.desktop" ];
+      "x-scheme-handler/http" = [ "chromium-desktop.desktop" ];
+      "x-scheme-handler/https" = [ "chromium-desktop.desktop" ];
+      "x-scheme-handler/about" = [ "chromium-desktop.desktop" ];
+      "x-scheme-handler/unknown" = [ "chromium-desktop.desktop" ];
+    };
   };
 }
