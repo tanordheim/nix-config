ee48e2ed2f43f119588af4d8367d5ff591607de3
trond <trond@nordheim.io>
2025-03-04 20:45:17 +0100

    674 current  2025-03-04 20:45:11  25.05.20250303.ba487db  6.12.12-asahi                          *

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -1,22 +1,28 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
-  home-manager.users.${config.username}.programs.nixvim.plugins.avante = {
-    enable = true;
-    settings = {
-      provider = "claude";
-      claude = {
-        endpoint = "https://api.anthropic.com";
-        model = "claude-3-5-sonnet-20241022";
-        temperature = 0;
-        max_tokens = 4096;
-      };
-      openai = {
-        endpoint = "https://api.openai.com/v1";
-        model = "gpt-4o";
-        timeout = 30000;
-        temperature = 0;
-        max_tokens = 4096;
+  home-manager.users.${config.username}.programs.nixvim = {
+    plugins.avante = {
+      enable = true;
+      settings = {
+        provider = "claude";
+        claude = {
+          endpoint = "https://api.anthropic.com";
+          model = "claude-3-5-sonnet-20241022";
+          temperature = 0;
+          max_tokens = 4096;
+        };
+        openai = {
+          endpoint = "https://api.openai.com/v1";
+          model = "gpt-4o";
+          timeout = 30000;
+          temperature = 0;
+          max_tokens = 4096;
+        };
       };
     };
+
+    extraPlugins = with pkgs.vimPlugins; [
+      img-clip-nvim
+    ];
   };
 }
