4c3c5e2fe689086fb43d963a609f1a0f3083e4d0
trond <trond@nordheim.io>
2025-03-04 20:48:01 +0100

    676 current  2025-03-04 20:47:54  25.05.20250303.ba487db  6.12.12-asahi                          *

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -7,7 +7,7 @@
         provider = "claude";
         claude = {
           endpoint = "https://api.anthropic.com";
-          model = "claude-3-7-sonnet";
+          model = "claude-3-7-sonnet-20250219";
           temperature = 0;
           max_tokens = 4096;
         };
