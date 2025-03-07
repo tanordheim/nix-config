c93d916b96b74f409cd6cff965c213d2cdcc35ea
trond <trond@nordheim.io>
2025-03-07 08:59:51 +0100

    680 current  2025-03-07 08:59:45  25.05.20250303.ba487db  6.12.12-asahi                          *

--- modules/common/node.nix
+++ modules/common/node.nix
@@ -1,7 +1,39 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    nodejs
-    pnpm
-  ];
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+with lib;
+let
+  cfg = config.node;
+
+  buildNpmConfig =
+    npmRegistries:
+    pkgs.writeText "npmrc" (
+      lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "${value.url}") npmRegistries)
+    );
+
+in
+{
+  options.node.npmRegistries = lib.mkOption {
+    type = types.attrsOf (
+      types.submodule {
+        options = {
+          url = mkOption {
+            type = types.str;
+          };
+        };
+      }
+    );
+    default = { };
+  };
+
+  config = {
+    environment.systemPackages = with pkgs; [
+      nodejs
+      pnpm
+    ];
+    home-manager.users.${config.username}.home.file.".npmrc".source = buildNpmConfig cfg.npmRegistries;
+  };
 }
