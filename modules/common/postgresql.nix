5fabc094f3941719d41e4fcbcea630d9d75ef477
trond <trond@nordheim.io>
2025-04-02 19:57:03 +0200

    741 current  2025-04-02 19:56:59  25.05.20250322.1e5b653  6.13.5-asahi                           *

--- modules/common/postgresql.nix
+++ modules/common/postgresql.nix
@@ -1,6 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   environment.systemPackages = with pkgs; [
     pgcli
   ];
+
+  home-manager.users.${config.username}.xdg.configFile = {
+    "pgcli/config" = {
+      text = ''
+        [main]
+        keyring = False
+      '';
+    };
+  };
 }
