572d11b7c4ad5f1e8f24718340382ce1c2898b10
trond <trond@nordheim.io>
2025-04-04 08:29:24 +0200

    743 current  2025-04-04 08:29:20  25.05.20250402.2c8d3f4  6.13.7-asahi                           *

--- modules/common/shell-aliases.nix
+++ modules/common/shell-aliases.nix
@@ -1,9 +1,23 @@
+{ config, ... }:
 {
-  environment.shellAliases = {
-    mkdir = "mkdir -p";
-    ".." = "cd ../";
-    "..." = "cd ../../";
-    "...." = "cd ../../../";
-    "....." = "cd ../../../../";
+  home-manager.users.${config.username} = {
+    home.shellAliases = {
+      mkdir = "mkdir -p";
+      ".." = "cd ../";
+      "..." = "cd ../../";
+      "...." = "cd ../../../";
+      "....." = "cd ../../../../";
+    };
+
+    programs.zsh.initExtra = # zsh
+      ''
+        # convenience function to convert dash-less guids to dashed guids
+        fixguid() {
+          echo $1 | sed "s/\(.\{8\}\)\(.\{4\}\)\(.\{4\}\)\(.\{4\}\)\(.\{12\}\)/\1-\2-\3-\4-\5/"
+        }
+        getguid() {
+          fixguid "$1" | clip
+        }
+      '';
   };
 }
--- modules/linux/shell-aliases.nix
+++ modules/linux/shell-aliases.nix
@@ -1,5 +1,7 @@
+{ config, ... }:
 {
-  environment.shellAliases = {
+  home-manager.users.${config.username}.home.shellAliases = {
+    clip = "wl-copy";
     open = "xdg-open";
   };
 }
