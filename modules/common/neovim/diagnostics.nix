cf4027f37334811570181040b395cc8d7dd7785e
trond <trond@nordheim.io>
2025-04-09 10:34:11 +0200

    747 current  2025-04-09 10:34:07  25.05.20250406.063dece  6.13.7-asahi                           *

--- modules/common/neovim/diagnostics.nix
+++ modules/common/neovim/diagnostics.nix
@@ -1,23 +1,39 @@
 { config, ... }:
 {
-  home-manager.users.${config.username}.programs.nixvim.extraConfigLua = # lua
-    ''
-      -- use virtual lines for diagnostic errors and virtual text for diagnostic warnings
-      vim.diagnostic.config({
-        virtual_text = {
-          current_line = true,
-          severity = {
-            min = vim.diagnostic.severity.INFO,
-            max = vim.diagnostic.severity.WARN,
+  home-manager.users.${config.username}.programs.nixvim = {
+    keymaps = [
+      {
+        key = "<leader>xo";
+        mode = "n";
+        action.__raw = # lua
+          ''
+            function()
+              vim.diagnostic.open_float(0, { scope = "line" })
+            end
+          '';
+        options.desc = "Open line diagnostics (float)";
+      }
+    ];
+
+    extraConfigLua = # lua
+      ''
+        -- use virtual lines for diagnostic errors and virtual text for diagnostic warnings
+        vim.diagnostic.config({
+          virtual_text = {
+            current_line = true,
+            severity = {
+              min = vim.diagnostic.severity.INFO,
+              max = vim.diagnostic.severity.WARN,
+            },
           },
-        },
-        virtual_lines = {
-          current_line = true,
-          severity = {
-            min = vim.diagnostic.severity.ERROR,
+          virtual_lines = {
+            current_line = true,
+            severity = {
+              min = vim.diagnostic.severity.ERROR,
+            },
           },
-        },
-        severity_sort = true
-      })
-    '';
+          severity_sort = true
+        })
+      '';
+  };
 }
