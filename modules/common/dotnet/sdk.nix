e46179f7885b84cb4105026cc0469a44a3575319
trond <trond@nordheim.io>
2025-04-11 14:18:57 +0200

    777 current  2025-04-11 14:18:53  25.05.20250408.c8cd814  6.13.7-asahi                          *

--- modules/common/dotnet/sdk.nix
+++ modules/common/dotnet/sdk.nix
@@ -1,28 +1,30 @@
 { config, pkgs, ... }:
 let
-  dotnet-packages =
-    with pkgs;
-    with dotnetCorePackages;
-    combinePackages [
-      # latest major versions
-      # dotnet.dotnetCorePackages.sdk_9_0
-      sdk_9_0
-      sdk_8_0
-
-      # specific older versions
-      sdk_9_0_1xx
-    ];
+  # dotnet-packages =
+  #   with pkgs;
+  #   with dotnetCorePackages;
+  #   combinePackages [
+  #     # latest major versions
+  #     # dotnet.dotnetCorePackages.sdk_9_0
+  #     sdk_9_0
+  #     sdk_8_0
+  #
+  #     # specific older versions
+  #     sdk_9_0_1xx
+  #   ];
 
 in
 {
-  environment.systemPackages = [ dotnet-packages ];
+  # environment.systemPackages = [ dotnet-packages ];
+  environment.systemPackages = [ pkgs.dotnetCorePackages.sdk_9_0 ];
 
   home-manager.users.${config.username}.home = {
     sessionPath = [
       "$HOME/.dotnet/tools"
     ];
     sessionVariables = {
-      DOTNET_ROOT = "${dotnet-packages}";
+      # DOTNET_ROOT = "${dotnet-packages}";
+      DOTNET_ROOT = "${pkgs.dotnetCorePackages.sdk_9_0}";
     };
   };
 }
