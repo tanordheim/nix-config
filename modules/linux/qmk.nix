c115214b4c0340b6b16cfaea57fafc76d6fbbe31
trond <trond@nordheim.io>
2025-05-16 11:56:36 +0200

    861 current  2025-05-16 11:56:31  25.05.20250510.d89fc19  6.14.4-asahi                          *

--- modules/linux/qmk.nix
+++ modules/linux/qmk.nix
@@ -2,10 +2,16 @@
 {
   environment.systemPackages = with pkgs; [
     qmk
-    qmk-udev-rules
   ];
 
-  services.udev.extraRules = ''
-    KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0142", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
-  '';
+  services.udev = {
+    packages = with pkgs; [
+      qmk-udev-rules
+    ];
+
+    # needed for VIA
+    extraRules = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3434", ATTRS{idProduct}=="0142", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+    '';
+  };
 }
