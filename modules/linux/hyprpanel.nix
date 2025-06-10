35923cd46c143f637263b8187ff212159f4a7fd0
trond <trond@nordheim.io>
2025-06-10 20:09:17 +0200

    970 current  2025-06-10 20:07:59  25.11.20250607.3e3afe5  6.14.8-asahi                          *

--- modules/linux/hyprpanel.nix
+++ modules/linux/hyprpanel.nix
@@ -13,6 +13,45 @@
       programs.hyprpanel = {
         enable = true;
         hyprland.enable = true;
+
+        settings = {
+          layout = {
+            "bar.layouts" = {
+              # internal monitor
+              "0" = {
+                left = [
+                  "dashboard"
+                  "workspaces"
+                  "netstat"
+                ];
+                middle = [ "windowtitle" ];
+                right = [
+                  "systray"
+                  "volume"
+                  "network"
+                  "kbinput"
+                  "battery"
+                  "clock"
+                  "notifications"
+                ];
+              };
+
+              # portrait monitor
+              "1" = {
+                left = [ "workspaces" ];
+                middle = [ ];
+                right = [ "windowtitle" ];
+              };
+
+              # main monitor
+              "2" = {
+                left = [ ];
+                middle = [ ];
+                right = [ ];
+              };
+            };
+          };
+        };
       };
     };
 }
