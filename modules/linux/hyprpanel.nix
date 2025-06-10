2aa3b49eb1153d41bfa2427a4aea87d9a9ec506a
trond <trond@nordheim.io>
2025-06-10 20:24:09 +0200

    975 current  2025-06-10 20:24:03  25.11.20250607.3e3afe5  6.14.8-asahi                          *

--- modules/linux/hyprpanel.nix
+++ modules/linux/hyprpanel.nix
@@ -52,7 +52,7 @@
           };
 
           theme.font.size = "1rem";
-          theme.font.weight = 400;
+          theme.font.weight = 500;
           theme.bar.scaling = 85;
           theme.bar.floating = true;
           theme.bar.buttons.enableBorders = true;
