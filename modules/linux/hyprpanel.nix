fae91773e07d03e6bd1a595fc4bb78d48a419baf
trond <trond@nordheim.io>
2025-06-10 20:32:20 +0200

    979 current  2025-06-10 20:32:15  25.11.20250607.3e3afe5  6.14.8-asahi                          *

--- modules/linux/hyprpanel.nix
+++ modules/linux/hyprpanel.nix
@@ -57,7 +57,7 @@
           theme.name = "catppuccin_mocha";
           theme.font.name = "${config.stylix.fonts.sansSerif.name}";
           theme.font.size = "${builtins.toString config.stylix.fonts.sizes.desktop}pt";
-          theme.font.weight = 500;
+          theme.font.weight = 600;
           # theme.bar.scaling = 85;
           theme.bar.floating = true;
           theme.bar.buttons.enableBorders = true;
