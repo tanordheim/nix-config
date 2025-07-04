6b90dcd6431ec956582e971c04cb5956f5e201d2
trond <trond@nordheim.io>
2025-07-04 16:14:52 +0200

    1062        2025-07-04 16:14:49  25.11.20250630.3016b4b  6.14.8-asahi  Unknown                 []              True

--- modules/common/jetbrains/rider.nix
+++ modules/common/jetbrains/rider.nix
@@ -26,13 +26,14 @@ let
         -Dawt.toolkit.name=WLToolkit
       '';
 
-  riderpkg = pkgs.jetbrains.rider.override {
-    libxml2 = pkgs.runCommand "libxml2.so.2" { } ''
-      install -Dm555                       \
-        ${pkgs.libxml2.out}/lib/libxml2.so \
-        $out/lib/libxml2.so.2
-    '';
-  };
+  riderpkg = pkgs.custom.jetbrains.rider;
+  # riderpkg = pkgs.jetbrains.rider.override {
+  #   libxml2 = pkgs.runCommand "libxml2.so.2" { } ''
+  #     install -Dm555                       \
+  #       ${pkgs.libxml2.out}/lib/libxml2.so \
+  #       $out/lib/libxml2.so.2
+  #   '';
+  # };
 in
 {
   home-manager.users.${config.username}.home = {
