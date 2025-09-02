250a08a4e544f2227ee69e9efe706bfc499e0263
trond <trond@nordheim.io>
2025-09-02 14:10:35 +0200

    562   2025-09-02 14:10:18   (current)

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -2,6 +2,8 @@
 {
   home-manager.users.${config.username}.programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+    matchBlocks."*" = { };
     matchBlocks."github.com" = {
       host = "github.com";
       hostname = "ssh.github.com";
