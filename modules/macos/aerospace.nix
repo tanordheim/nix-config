cc3e9f6228bddc36a4437898e18ec5422fdce134
trond <trond@nordheim.io>
2025-09-16 22:28:00 +0200

    602   2025-09-16 22:27:43   (current)

--- modules/macos/aerospace.nix
+++ modules/macos/aerospace.nix
@@ -60,7 +60,7 @@ in
           # 'after-startup-command' is run after 'after-login-command'
           # Available commands : https://nikitabobko.github.io/AeroSpace/commands
           after-startup-command = [
-            "exec-and-forget borders active_color=0xff${colors.base0D} inactive_color=0xff${colors.base03} width=5.0"
+            "exec-and-forget ${pkgs.jankyborders}/bin/borders active_color=0xff${colors.base0D} inactive_color=0xff${colors.base03} width=5.0"
           ];
 
           # Start AeroSpace at login
