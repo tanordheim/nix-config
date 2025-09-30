e86e1ce999325d850bf06bbf39df0b055bc5b9ad
trond <trond@nordheim.io>
2025-09-30 09:53:16 +0200

    623   2025-09-30 09:52:57   (current)

--- modules/common/neovim/plugins/avante.nix
+++ modules/common/neovim/plugins/avante.nix
@@ -9,22 +9,23 @@
         };
       };
       settings = {
-        provider = "openai";
+        provider = "claude";
         providers = {
           claude = {
             endpoint = "https://api.anthropic.com";
-            model = "claude-3-5-sonnet-20241022";
+            model = "claude-3-7-sonnet-20250219";
             extra_request_body = {
-              temperature = 0;
-              max_tokens = 4096;
+              temperature = 0.75;
+              max_tokens = 20480;
             };
           };
           openai = {
             endpoint = "https://api.openai.com/v1";
-            model = "gpt-5";
+            model = "gpt-4o";
             timeout = 30000;
             extra_request_body = {
-              temperature = 1;
+              temperature = 0;
+              max_tokens = 4096;
             };
           };
         };
