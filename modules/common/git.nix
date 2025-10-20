6eb1632b43a4a177839cb8a0341f55348e033dd9
trond <trond@nordheim.io>
2025-10-20 21:56:44 +0200

    633   2025-10-20 21:52:51   (current)

--- modules/common/git.nix
+++ modules/common/git.nix
@@ -10,57 +10,62 @@
     programs.git = {
       enable = true;
 
-      userName = config.username;
-      userEmail = config.git.email;
-
       signing = {
         signByDefault = true;
         key = config.user.ssh.key;
       };
 
-      aliases = {
-        st = "status";
-        co = "checkout";
-        di = "diff";
-        dc = "diff --cached";
-        amend = "commit --amend";
-        aa = "add --all :/";
-        b = "branch";
-        lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-        pr = "pull --rebase";
-        thrust = "!f() { until git push $@; do sleep 0.5; done; }; f";
+      settings = {
+        alias = {
+          st = "status";
+          co = "checkout";
+          di = "diff";
+          dc = "diff --cached";
+          amend = "commit --amend";
+          aa = "add --all :/";
+          b = "branch";
+          lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+          pr = "pull --rebase";
+          thrust = "!f() { until git push $@; do sleep 0.5; done; }; f";
+        };
+
+        user = {
+          name = config.username;
+          email = config.git.email;
+        };
+
+        extraConfig = {
+          apply.whitespace = "nowarn";
+          branch.autosetupmerge = true;
+          color = {
+            branch = "auto";
+            diff = "auto";
+            interactive = "auto";
+            status = "auto";
+          };
+          core = {
+            pager = "less -FXRS -x2";
+            autocrlf = "input";
+            editor = "nvim";
+          };
+          "filter \"lfs\"" = {
+            clean = "${pkgs.git-lfs}/bin/git-lfs clean -- %f";
+            smudge = "${pkgs.git-lfs}/bin/git-lfs smudge --skip -- %f";
+            process = "${pkgs.git-lfs}/bin/git-lfs filter-process --skip";
+            required = true;
+          };
+          github.user = config.git.githubUsername;
+          init.defaultBranch = "main";
+          merge.tool = "vimdiff";
+          push.default = "simple";
+          rebase.autosquash = true;
+          rerere = {
+            enabled = true;
+            autoupdate = true;
+          };
+        };
       };
 
-      extraConfig = {
-        apply.whitespace = "nowarn";
-        branch.autosetupmerge = true;
-        color = {
-          branch = "auto";
-          diff = "auto";
-          interactive = "auto";
-          status = "auto";
-        };
-        core = {
-          pager = "less -FXRS -x2";
-          autocrlf = "input";
-          editor = "nvim";
-        };
-        "filter \"lfs\"" = {
-          clean = "${pkgs.git-lfs}/bin/git-lfs clean -- %f";
-          smudge = "${pkgs.git-lfs}/bin/git-lfs smudge --skip -- %f";
-          process = "${pkgs.git-lfs}/bin/git-lfs filter-process --skip";
-          required = true;
-        };
-        github.user = config.git.githubUsername;
-        init.defaultBranch = "main";
-        merge.tool = "vimdiff";
-        push.default = "simple";
-        rebase.autosquash = true;
-        rerere = {
-          enabled = true;
-          autoupdate = true;
-        };
-      };
     };
   };
 }
