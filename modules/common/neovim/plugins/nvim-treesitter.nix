d35c207cc001600c35f715f81c3408520524a4ec
trond <trond@nordheim.io>
2025-10-31 10:19:14 +0100

    638   2025-10-31 10:17:58   (current)

--- modules/common/neovim/plugins/nvim-treesitter.nix
+++ modules/common/neovim/plugins/nvim-treesitter.nix
@@ -41,49 +41,51 @@
       ];
     };
     treesitter-textobjects = {
+      settings = {
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "af" = {
+              query = "@function.outer";
+              desc = "Select outer part of function";
+            };
+            "if" = {
+              query = "@function.inner";
+              desc = "Select outer part of function";
+            };
+            "ac" = {
+              query = "@class.outer";
+              desc = "Select outer part of class";
+            };
+            "ic" = {
+              query = "@class.inner";
+              desc = "Select inner part of class";
+            };
+          };
+        };
+        move = {
+          enable = true;
+          setJumps = true;
+          gotoNextStart = {
+            "]m" = "@function.outer";
+            "]]" = "@class.outer";
+          };
+          gotoNextEnd = {
+            "]M" = "@function.outer";
+            "][" = "@class.outer";
+          };
+          gotoPreviousStart = {
+            "[m" = "@function.outer";
+            "[[" = "@class.outer";
+          };
+          gotoPreviousEnd = {
+            "[M" = "@function.outer";
+            "[]" = "@class.outer";
+          };
+        };
+      };
       enable = true;
-      select = {
-        enable = true;
-        lookahead = true;
-        keymaps = {
-          "af" = {
-            query = "@function.outer";
-            desc = "Select outer part of function";
-          };
-          "if" = {
-            query = "@function.inner";
-            desc = "Select outer part of function";
-          };
-          "ac" = {
-            query = "@class.outer";
-            desc = "Select outer part of class";
-          };
-          "ic" = {
-            query = "@class.inner";
-            desc = "Select inner part of class";
-          };
-        };
-      };
-      move = {
-        enable = true;
-        setJumps = true;
-        gotoNextStart = {
-          "]m" = "@function.outer";
-          "]]" = "@class.outer";
-        };
-        gotoNextEnd = {
-          "]M" = "@function.outer";
-          "][" = "@class.outer";
-        };
-        gotoPreviousStart = {
-          "[m" = "@function.outer";
-          "[[" = "@class.outer";
-        };
-        gotoPreviousEnd = {
-          "[M" = "@function.outer";
-          "[]" = "@class.outer";
-        };
-      };
     };
   };
 }
