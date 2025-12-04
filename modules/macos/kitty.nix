c8aefcc15fe5f97f4d74dead98c13e27766496f5
trond <trond@nordheim.io>
2025-12-04 23:15:55 +0100

    43   2025-12-04 23:15:48   (current)

--- modules/macos/kitty.nix
+++ modules/macos/kitty.nix
@@ -3,5 +3,6 @@
   home-manager.users.${config.username}.programs.kitty.keybindings = {
     "cmd+c" = "copy_and_clear_or_interrupt";
     "cmd+v" = "paste_from_clipboard";
+    "cmd+q" = "quit";
   };
 }
