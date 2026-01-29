2960cd8e67b159f59d5fdfe3ba7b75beda8ec835
trond <trond@nordheim.io>
2026-01-29 09:16:25 +0100

    66   2026-01-29 09:16:20   (current)

--- modules/common/neovim/plugins/languages/nix.nix
+++ modules/common/neovim/plugins/languages/nix.nix
@@ -5,9 +5,11 @@
       nixfmt
     ];
 
-    plugins.treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
-      nix
-    ];
+    # broken by https://github.com/nix-community/tree-sitter-nix/issues/84
+    # TODO: find fix
+    # plugins.treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+    #   nix
+    # ];
 
     plugins.conform-nvim.settings = {
       formatters_by_ft.nix = [ "nixfmt" ];
