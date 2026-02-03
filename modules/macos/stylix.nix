ea0b32567488327e6a2747979fc57cf9596f095f
trond <trond@nordheim.io>
2026-02-03 14:58:17 +0100

    72   2026-02-03 14:58:05   (current)

--- modules/macos/stylix.nix
+++ modules/macos/stylix.nix
@@ -1,4 +1,21 @@
 { pkgs, config, ... }:
 {
   stylix.fonts.sizes.terminal = 15;
+
+  homebrew.casks = [
+    "desktoppr"
+  ];
+
+  launchd.user.agents.stylixwallpaper = {
+    serviceConfig = {
+      Label = "set-stylix-wallpaper";
+      ProgramArguments = [
+        "/opt/homebrew/bin/desktoppr"
+        (toString config.stylix.image)
+      ];
+      RunAtLoad = true;
+      StandardOutPath = "/tmp/set-stylix-wallpaper.log";
+      StandardErrorPath = "/tmp/set-stylix-wallpaper.error.log";
+    };
+  };
 }
