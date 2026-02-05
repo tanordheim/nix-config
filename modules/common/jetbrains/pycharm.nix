97a829028e79c72a51d989ca3ad2dc73caf37a09
trond <trond@nordheim.io>
2026-02-05 09:54:54 +0100

    78   2026-02-05 09:54:47   (current)

--- modules/common/jetbrains/pycharm.nix
+++ modules/common/jetbrains/pycharm.nix
@@ -7,30 +7,30 @@
 let
   versionMajorMinor = lib.versions.majorMinor pkgs.jetbrains.pycharm.version;
 
-  # vmOptionsFile =
-  #   if pkgs.stdenv.isDarwin then
-  #     "Library/Application Support/JetBrains/Pycharm${versionMajorMinor}/rider.vmoptions"
-  #   else
-  #     ".config/JetBrains/Pycharm${versionMajorMinor}/rider64.vmoptions";
-  #
-  # vmOptionsContent =
-  #   if pkgs.stdenv.isDarwin then
-  #     ''
-  #       -Xms1g
-  #       -Xmx2g
-  #     ''
-  #   else
-  #     ''
-  #       -Xms1g
-  #       -Xmx2g
-  #       -Dawt.toolkit.name=WLToolkit
-  #     '';
+  vmOptionsFile =
+    if pkgs.stdenv.isDarwin then
+      "Library/Application Support/JetBrains/Pycharm${versionMajorMinor}/pycharm.vmoptions"
+    else
+      ".config/JetBrains/Pycharm${versionMajorMinor}/pycharm.vmoptions";
+
+  vmOptionsContent =
+    if pkgs.stdenv.isDarwin then
+      ''
+        -Xms1g
+        -Xmx2g
+      ''
+    else
+      ''
+        -Xms1g
+        -Xmx2g
+        -Dawt.toolkit.name=WLToolkit
+      '';
 in
 {
   home-manager.users.${config.username}.home = {
     packages = with pkgs; [ jetbrains.pycharm ];
-    # file = {
-    #   "${vmOptionsFile}".text = vmOptionsContent;
-    # };
+    file = {
+      "${vmOptionsFile}".text = vmOptionsContent;
+    };
   };
 }
