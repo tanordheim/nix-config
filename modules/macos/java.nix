cc0657e51fc4ce528e869d0fc2e3659a5045d762
trond <trond@nordheim.io>
2026-02-10 09:29:19 +0100

    84   2026-02-10 09:29:11   (current)

--- modules/macos/java.nix
+++ modules/macos/java.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   launchd.user.envVariables = {
     JAVA_HOME = "${pkgs.jdk.home}";
+  };
+
+  launchd.agents.setenv-java-home = {
+    serviceConfig = {
+      Label = "org.nix.setenv-java-home";
+      ProgramArguments = [
+        "/bin/launchctl"
+        "setenv"
+        "JAVA_HOME"
+        "${pkgs.jdk.home}"
+      ];
+      RunAtLoad = true;
+      StandardOutPath = "/dev/null";
+      StandardErrorPath = "/dev/null";
+    };
   };
 }
