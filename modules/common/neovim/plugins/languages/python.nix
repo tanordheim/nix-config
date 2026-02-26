733aa5ad43a24cd0047aef529cbb34cb38c88280
trond <trond@nordheim.io>
2026-02-26 14:54:34 +0100

    121   2026-02-26 14:54:24   (current)

--- modules/common/neovim/plugins/languages/python.nix
+++ modules/common/neovim/plugins/languages/python.nix
@@ -2,26 +2,26 @@
 {
   home-manager.users.${config.username}.programs.nixvim = {
     extraPackages = with pkgs; [
-      basedpyright
-      black
+      ty
+      ruff
     ];
 
     plugins.treesitter.grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
       python
     ];
 
-    plugins.lsp.servers.basedpyright = {
+    plugins.lsp.servers.ty = {
       enable = true;
       cmd = [
-        "${pkgs.basedpyright}/bin/basedpyright-langserver"
-        "--stdio"
+        "${pkgs.ty}/bin/ty"
+        "server"
       ];
     };
 
     plugins.conform-nvim.settings = {
-      formatters_by_ft.python = [ "black" ];
-      formatters.black = {
-        command = "${pkgs.black}/bin/black";
+      formatters_by_ft.python = [ "ruff_format" ];
+      formatters.ruff_format = {
+        command = "${pkgs.ruff}/bin/ruff";
       };
     };
   };
