2be9b8b69b7b9c576009af49900c21573baa8e72
trond <trond@nordheim.io>
2026-03-05 16:24:46 +0100

    159   2026-03-05 16:24:39   (current)

--- modules/common/neovim/default.nix
+++ modules/common/neovim/default.nix
@@ -9,15 +9,18 @@
   home-manager.users.${config.username} =
     { config, ... }:
     {
+      home.shellAliases = {
+        vim = "nvim";
+        vi = "nvim";
+        vimdiff = "nvim -d";
+      };
+
       imports = [
         nixvim.homeModules.nixvim
       ];
       programs.nixvim = {
         enable = true;
         defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
-        vimdiffAlias = true;
 
         # set <space> as the leader key; this must happen before anything else
         globals.mapleader = " ";
