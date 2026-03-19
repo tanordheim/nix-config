f98c4b60808f0b11ee6b3e8acf78b204d7fea7c7
trond <trond@nordheim.io>
2026-03-19 13:04:29 +0100

    174   2026-03-19 13:04:22   (current)

--- overlays/roslyn-ls/default.nix
+++ overlays/roslyn-ls/default.nix
@@ -1,18 +1,11 @@
 { ... }:
 final: prev: {
   roslyn-ls = prev.roslyn-ls.override {
-    dotnet-sdk =
-      with prev.dotnetCorePackages;
-      sdk_10_0-bin
-      // {
-        inherit
-          (combinePackages [
-            sdk_9_0-bin
-            sdk_8_0-bin
-          ])
-          packages
-          targetPackages
-          ;
-      };
+    # override the .net SDK dependencies to use prebuilt binaries instead of source packages to not have installing roslyn-ls take 7 years
+    dotnetCorePackages = prev.dotnetCorePackages // {
+      sdk_8_0 = prev.dotnetCorePackages.sdk_8_0-bin;
+      sdk_9_0 = prev.dotnetCorePackages.sdk_9_0-bin;
+      sdk_10_0 = prev.dotnetCorePackages.sdk_10_0-bin;
+    };
   };
 }
