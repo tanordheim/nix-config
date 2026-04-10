36aaae7c66af697169dbb6e6236a7a8cca00503f
trond <trond@nordheim.io>
2026-04-10 17:31:06 +0200

    update hsrv config

--- hosts/hsrv/default.nix
+++ hosts/hsrv/default.nix
@@ -10,8 +10,23 @@
     ../common/users/trond.nix
   ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
   nixpkgs.hostPlatform = "x86_64-linux";
-  system.stateVersion = "24.11";
+  system.stateVersion = "25.11";
   networking.hostName = "hsrv";
-  home-manager.users.trond.home.stateVersion = "24.11";
+  networking.interfaces.eno1.ipv4.addresses = [
+    {
+      address = "192.168.69.10";
+      prefixLength = 24;
+    }
+  ];
+  networking.defaultGateway = "192.168.69.1";
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+
+  home-manager.users.trond.home.stateVersion = "25.11";
 }
