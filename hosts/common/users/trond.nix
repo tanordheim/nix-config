00e5501bdfdddac3a72cda0bc2a723ac8bf01ce8
trond <trond@nordheim.io>
2026-04-10 17:45:41 +0200

    add authorized key

--- hosts/common/users/trond.nix
+++ hosts/common/users/trond.nix
@@ -17,6 +17,9 @@ in
   // lib.optionalAttrs pkgs.stdenv.isLinux {
     isNormalUser = true;
     extraGroups = [ "wheel" ] ++ ifTheyExist [ "docker" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDG70eJKWOrmTNDfBWLc8+EeniRAOvgfV6bSUfSvkLN4NWc/bWVNlIAiLU24Nievcb8nxgkBLyDcY8ireeCktfMUSmZTr3Zfr8Umd/4DgvoRBQEwLPJGplIqCrzCjuxNxZSRmZnkbsptf0lEFRYgn/9InhCC8ZSk7I4pR0RvPFvw4wjRSe9SBOR5n0ig79D03r31koPwpiDBl0QHUpfnvIg5BpQ9pCNse6Hz1dhjuupE9M0wStUiyPS25fXJjwLDNvXAA54utImivHWa2CAHsY2mmyymwchYq3nqaC6NsRNsNGewQW+DKF9/Xlc0HPKbYoMMM0hQ9uxC6LoOY496MTX"
+    ];
   };
 
   home-manager.users.trond = {
