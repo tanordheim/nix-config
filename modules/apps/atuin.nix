d98988544d05a1a946d4e18b25b627994e8326b9
trond <trond@nordheim.io>
2026-04-27 11:00:28 +0200

    309   2026-04-27 11:00:20   (current)

--- modules/apps/atuin.nix
+++ modules/apps/atuin.nix
@@ -18,6 +18,14 @@
               "^.*(API_KEY|SECRET|TOKEN|PASSWORD)="
               "^op (run|read)"
             ];
+            style = "compact";
+            inline_height = 25;
+            invert = true;
+            show_preview = true;
+            show_help = false;
+            enter_accept = false;
+            keymap_mode = "vim-normal";
+            filter_mode = "global";
           };
         };
       };
