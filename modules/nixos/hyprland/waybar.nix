a413dc6f0bae6bf9c5e7fa06302470f2ca69ef01
trond <trond@nordheim.io>
2026-05-01 20:23:17 +0200

    23          2026-05-01 20:23:16  26.05.20260418.b12141e    7.0.0    Unknown                 []              True

--- modules/nixos/hyprland/waybar.nix
+++ modules/nixos/hyprland/waybar.nix
@@ -8,6 +8,30 @@
         playerctl = "${pkgs.playerctl}/bin/playerctl";
         swayncClient = "${pkgs.swaynotificationcenter}/bin/swaync-client";
 
+        fa = code: builtins.fromJSON ''"\u${code}"'';
+
+        glyph = {
+          cpu = fa "F4BC";
+          memory = fa "F2DB";
+          disk = "󰋊";
+          temp = fa "F2C9";
+          gpu = "󰢮";
+          nvme = fa "F0A0";
+          dimm = fa "F2DB";
+          volume = fa "F028";
+          mute = fa "F026";
+          wifi = fa "F1EB";
+          ethernet = "󰈀";
+          wifiOff = "󰖪";
+          bluetooth = fa "F293";
+          bolt = fa "F0E7";
+          moon = fa "F186";
+          play = fa "F04B";
+          pause = fa "F04C";
+          stop = fa "F04D";
+          spotify = fa "F1BC";
+        };
+
         icon = g: "<span size='large'>${g}</span>";
 
         commonBar = {
@@ -114,13 +138,13 @@
                 format = "{player_icon} {dynamic}";
                 format-paused = "{status_icon} <i>{dynamic}</i>";
                 player-icons = {
-                  default = icon "▶";
-                  spotify = icon "";
+                  default = icon glyph.play;
+                  spotify = icon glyph.spotify;
                 };
                 status-icons = {
-                  paused = icon "⏸";
-                  playing = icon "▶";
-                  stopped = icon "⏹";
+                  paused = icon glyph.pause;
+                  playing = icon glyph.play;
+                  stopped = icon glyph.stop;
                 };
                 dynamic-len = 30;
               };
@@ -128,8 +152,8 @@
               idle_inhibitor = {
                 format = "{icon}";
                 format-icons = {
-                  activated = icon "⚡";
-                  deactivated = icon "⏾";
+                  activated = icon glyph.bolt;
+                  deactivated = icon glyph.moon;
                 };
               };
 
@@ -139,15 +163,15 @@
               };
 
               cpu = {
-                format = "${icon ""} {usage}%";
+                format = "${icon glyph.cpu} {usage}%";
                 interval = 5;
               };
               memory = {
-                format = "${icon ""} {percentage}%";
+                format = "${icon glyph.memory} {percentage}%";
                 interval = 5;
               };
               disk = {
-                format = "${icon "󰋊"} {percentage_used}%";
+                format = "${icon glyph.disk} {percentage_used}%";
                 path = "/";
                 interval = 30;
               };
@@ -156,49 +180,49 @@
                 hwmon-path-abs = "/sys/devices/pci0000:00/0000:00:18.3/hwmon";
                 input-filename = "temp1_input";
                 critical-threshold = 85;
-                format = "${icon ""} {temperatureC}°";
+                format = "${icon glyph.temp} {temperatureC}°";
                 tooltip = false;
               };
               "temperature#gpu" = {
                 hwmon-path-abs = "/sys/devices/pci0000:00/0000:00:01.1/0000:01:00.0/0000:02:00.0/0000:03:00.0/hwmon";
                 input-filename = "temp2_input";
                 critical-threshold = 95;
-                format = "${icon "󰢮"} {temperatureC}°";
+                format = "${icon glyph.gpu} {temperatureC}°";
                 tooltip = false;
               };
               "temperature#nvme" = {
                 hwmon-path-abs = "/sys/devices/pci0000:00/0000:00:02.2/0000:0c:00.0/nvme/nvme0/hwmon";
                 input-filename = "temp1_input";
                 critical-threshold = 70;
-                format = "${icon ""} {temperatureC}°";
+                format = "${icon glyph.nvme} {temperatureC}°";
                 tooltip = false;
               };
               "temperature#dimm" = {
                 hwmon-path-abs = "/sys/devices/pci0000:00/0000:00:14.0/i2c-0/0-0053/hwmon";
                 input-filename = "temp1_input";
                 critical-threshold = 75;
-                format = "${icon ""} {temperatureC}°";
+                format = "${icon glyph.dimm} {temperatureC}°";
                 tooltip = false;
               };
 
               wireplumber = {
-                format = "${icon ""} {volume}%";
-                format-muted = "${icon ""} muted";
+                format = "${icon glyph.volume} {volume}%";
+                format-muted = "${icon glyph.mute} muted";
                 on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
               };
 
               bluetooth = {
-                format = "${icon ""} {status}";
-                format-disabled = "${icon ""} off";
-                format-connected = "${icon ""} {device_alias}";
+                format = "${icon glyph.bluetooth} {status}";
+                format-disabled = "${icon glyph.bluetooth} off";
+                format-connected = "${icon glyph.bluetooth} {device_alias}";
                 tooltip-format = "{controller_alias}\n{num_connections} connected";
                 on-click = "${pkgs.blueman}/bin/blueman-manager";
               };
 
               network = {
-                format-wifi = "${icon ""} {essid}";
-                format-ethernet = "${icon "󰈀"} {ifname}";
-                format-disconnected = "${icon "󰖪"} disconnected";
+                format-wifi = "${icon glyph.wifi} {essid}";
+                format-ethernet = "${icon glyph.ethernet} {ifname}";
+                format-disconnected = "${icon glyph.wifiOff} disconnected";
                 tooltip-format = "{ifname}: {ipaddr}";
                 on-click = "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
               };
