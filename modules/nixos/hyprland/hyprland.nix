cfec677dfa9f72fb8f004178ba9e9f851b1321dc
trond <trond@nordheim.io>
2026-05-02 17:48:00 +0200

    47          2026-05-02 17:47:58  26.05.20260430.15f4ee4    7.0.3    Unknown                 []              True

--- modules/nixos/hyprland/hyprland.nix
+++ modules/nixos/hyprland/hyprland.nix
@@ -194,11 +194,16 @@
               "3, monitor:DP-2"
               "4, monitor:DP-2"
               "5, monitor:DP-2"
-              "6, monitor:DP-1, default:true"
-              "7, monitor:DP-1"
-              "8, monitor:DP-1"
-              "9, monitor:DP-1"
-              "10, monitor:DP-1"
+              "6, monitor:DP-1, default:true, layoutopt:orientation:top"
+              "7, monitor:DP-1, layoutopt:orientation:top"
+              "8, monitor:DP-1, layoutopt:orientation:top"
+              "9, monitor:DP-1, layoutopt:orientation:top"
+              "10, monitor:DP-1, layoutopt:orientation:top"
+            ];
+
+            windowrulev2 = [
+              "workspace 6 silent, class:^(Slack)$"
+              "workspace 6 silent, class:^(org.telegram.desktop)$"
             ];
           };
         };
