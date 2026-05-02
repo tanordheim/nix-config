aa78ac3a3f6adc76a11ff24f6b2f2f10cc58d7f1
trond <trond@nordheim.io>
2026-05-02 19:12:00 +0200

    53          2026-05-02 18:13:01  26.05.20260430.15f4ee4    7.0.3    Unknown                 []              True

--- modules/common/neovim/colorscheme.nix
+++ modules/common/neovim/colorscheme.nix
@@ -1,29 +1,18 @@
-{ lib, config, pkgs, ... }:
+{ lib, config, ... }:
     {
 
         # stylix interferes a little with how I want neovim to look
         stylix.targets.nixvim.enable = false;
 
         programs.nixvim = {
-          colorscheme = "base46-catppuccin";
-          extraPlugins = [
-            (pkgs.vimUtils.buildVimPlugin {
-              pname = "base46";
-              version = "v3.0-65cb8ea";
-              src = pkgs.fetchFromGitHub {
-                owner = "AvengeMedia";
-                repo = "base46";
-                rev = "65cb8ea00157a2808de594def4c70071255132cd";
-                hash = "sha256-BlKW8BN4VBi4CQpwOjEjisFfuLbHBGRfA1Ll4De6IPM=";
-              };
-              nvimRequireCheck = [ "base46" ];
-            })
-          ];
-          extraConfigLua = ''
-            require("base46").setup({
-              transparency = true,
-            })
-          '';
+          colorschemes.catppuccin = {
+            enable = true;
+            settings = {
+              flavour = "mocha";
+              dim_inactive.enabled = true;
+              transparent_background = true;
+            };
+          };
         };
 
     }
