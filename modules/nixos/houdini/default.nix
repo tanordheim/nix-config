1326bb86ee2eac3cba22d6a46b17124bb10e075c
trond <trond@nordheim.io>
2026-05-05 17:53:56 +0200

    91          2026-05-05 17:53:54  26.05.20260430.15f4ee4    7.0.3    Unknown                 []              True

--- modules/nixos/houdini/default.nix
+++ modules/nixos/houdini/default.nix
@@ -4,6 +4,17 @@
       { pkgs, ... }:
       {
         home.packages = [ pkgs.houdini ];
+
+        xdg.desktopEntries.houdini = {
+          name = "Houdini";
+          genericName = "3D Animation";
+          comment = "SideFX Houdini";
+          exec = "houdini %F";
+          icon = "${pkgs.houdini.unwrapped}/houdini_logo.png";
+          terminal = false;
+          categories = [ "Graphics" "3DGraphics" ];
+          mimeType = [ "application/x-houdini" ];
+        };
       }
     )
   ];
