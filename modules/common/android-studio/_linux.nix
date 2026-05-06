fd51b2c8b0ae765026c41181437a564ede18f324
trond <trond@nordheim.io>
2026-05-06 17:56:11 +0200

    107         2026-05-06 17:56:09  26.05.20260430.15f4ee4    7.0.3    Unknown                 []              True

--- modules/common/android-studio/_linux.nix
+++ modules/common/android-studio/_linux.nix
@@ -1,10 +1,32 @@
+{ pkgs, ... }:
+let
+  androidSdk =
+    (pkgs.androidenv.composeAndroidPackages {
+      platformVersions = [
+        "35"
+        "34"
+      ];
+      buildToolsVersions = [ "34.0.0" ];
+      includeEmulator = false;
+      includeNDK = false;
+      includeSources = false;
+      includeSystemImages = false;
+    }).androidsdk;
+  androidHome = "${androidSdk}/libexec/android-sdk";
+in
 {
+  nixpkgs.config.android_sdk.accept_license = true;
+
   home-manager.sharedModules = [
-    (
-      { pkgs, ... }:
-      {
-        home.packages = [ pkgs.android-studio ];
-      }
-    )
+    {
+      home.packages = [
+        pkgs.android-studio
+        androidSdk
+      ];
+      home.sessionVariables = {
+        ANDROID_HOME = androidHome;
+        ANDROID_SDK_ROOT = androidHome;
+      };
+    }
   ];
 }
