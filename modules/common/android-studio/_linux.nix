391ea2aca4b2ad165bcae7134df3921680f2681c
trond <trond@nordheim.io>
2026-05-07 11:55:09 +0200

    322   2026-05-07 11:55:02   (current)

--- modules/common/android-studio/_linux.nix
+++ modules/common/android-studio/_linux.nix
@@ -1,33 +1,8 @@
 { pkgs, ... }:
-let
-  androidSdk =
-    (pkgs.androidenv.composeAndroidPackages {
-      numLatestPlatformVersions = 3;
-      buildToolsVersions = [
-        "35.0.0"
-        "36.0.0"
-        "37.0.0"
-      ];
-      includeEmulator = false;
-      includeNDK = false;
-      includeSources = false;
-      includeSystemImages = false;
-    }).androidsdk;
-  androidHome = "${androidSdk}/libexec/android-sdk";
-in
 {
-  nixpkgs.config.android_sdk.accept_license = true;
-
   home-manager.sharedModules = [
     {
-      home.packages = [
-        pkgs.android-studio
-        androidSdk
-      ];
-      home.sessionVariables = {
-        ANDROID_HOME = androidHome;
-        ANDROID_SDK_ROOT = androidHome;
-      };
+      home.packages = [ pkgs.android-studio ];
     }
   ];
 }
--- modules/common/android-studio/default.nix
+++ modules/common/android-studio/default.nix
@@ -1,4 +1,37 @@
-{ lib, isDarwin, ... }:
+{
+  lib,
+  pkgs,
+  isDarwin,
+  ...
+}:
+let
+  androidSdk =
+    (pkgs.androidenv.composeAndroidPackages {
+      numLatestPlatformVersions = 3;
+      buildToolsVersions = [
+        "35.0.0"
+        "36.0.0"
+        "37.0.0"
+      ];
+      includeEmulator = false;
+      includeNDK = false;
+      includeSources = false;
+      includeSystemImages = false;
+    }).androidsdk;
+  androidHome = "${androidSdk}/libexec/android-sdk";
+in
 {
   imports = [ (lib.mkPlatformImport ./. isDarwin) ];
+
+  nixpkgs.config.android_sdk.accept_license = true;
+
+  home-manager.sharedModules = [
+    {
+      home.packages = [ androidSdk ];
+      home.sessionVariables = {
+        ANDROID_HOME = androidHome;
+        ANDROID_SDK_ROOT = androidHome;
+      };
+    }
+  ];
 }
