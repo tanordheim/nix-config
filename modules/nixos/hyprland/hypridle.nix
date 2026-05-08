2f62ccba2edc7770c9fed8d1c4b23c2992fde1fa
trond <trond@nordheim.io>
2026-05-08 08:59:37 +0200

    116         2026-05-08 08:59:36  26.05.20260505.549bd84    7.0.3    Unknown                 []              True

--- modules/nixos/hyprland/hypridle.nix
+++ modules/nixos/hyprland/hypridle.nix
@@ -5,17 +5,18 @@
         enable = true;
         settings = {
           general = {
+            lock_cmd = "pidof hyprlock || hyprlock";
             before_sleep_cmd = "loginctl lock-session";
             after_sleep_cmd = "hyprctl dispatch dpms on";
             ignore_dbus_inhibit = false;
           };
           listener = [
             {
-              timeout = 300;
+              timeout = 1800;
               on-timeout = "loginctl lock-session";
             }
             {
-              timeout = 600;
+              timeout = 2100;
               on-timeout = "hyprctl dispatch dpms off";
               on-resume = "hyprctl dispatch dpms on";
             }
