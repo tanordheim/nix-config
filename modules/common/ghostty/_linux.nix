ec1e17668ff9a49096979d4fbf4fcd89749b8ea1
trond <trond@nordheim.io>
2026-05-11 11:19:33 +0200

    126         2026-05-11 03:52:43  26.05.20260505.549bd84    7.0.3    Unknown                 []              True

--- modules/common/ghostty/_linux.nix
+++ modules/common/ghostty/_linux.nix
@@ -11,12 +11,13 @@
           confirm-close-surface = false;
           mouse-hide-while-typing = true;
           shell-integration-features = "ssh-terminfo";
+          app-notifications = "no-clipboard-copy,no-config-reload,no-prefix-key";
 
           keybind = [
             "ctrl+shift+k=increase_font_size:1"
             "ctrl+shift+j=decrease_font_size:1"
             "ctrl+shift+n=reset_font_size"
-            "ctrl+a>q=reload_config"
+            "ctrl+space>q=reload_config"
           ];
         };
       };
