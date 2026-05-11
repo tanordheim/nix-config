05d870a6c8495858460b0437e7300e54621f0ed6
trond <trond@nordheim.io>
2026-05-11 20:12:26 +0200

    131         2026-05-11 20:12:18  26.05.20260505.549bd84    7.0.3    Unknown                 []              True

--- modules/common/docker/_linux.nix
+++ modules/common/docker/_linux.nix
@@ -1,3 +1,12 @@
 {
   virtualisation.docker.enable = true;
+
+  networking.firewall.extraCommands = ''
+    iptables -I nixos-fw 1 -i docker0 -j nixos-fw-accept
+    iptables -I nixos-fw 1 -i br+ -j nixos-fw-accept
+  '';
+  networking.firewall.extraStopCommands = ''
+    iptables -D nixos-fw -i docker0 -j nixos-fw-accept 2>/dev/null || true
+    iptables -D nixos-fw -i br+ -j nixos-fw-accept 2>/dev/null || true
+  '';
 }
