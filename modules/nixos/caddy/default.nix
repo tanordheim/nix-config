506dd55433a83c605111b41316e23b09f83752a3
trond <trond@nordheim.io>
2026-05-18 21:56:20 +0200

    fix caddy hash

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -1,34 +1,34 @@
 {
-      lib,
-      config,
-      pkgs,
-      ...
-    }:
-    {
-      
-        services.caddy = {
-          enable = true;
-          package = pkgs.caddy.withPlugins {
-            plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-            hash = "sha256-Olz4W84Kiyldy+JtbIicVCL7dAYl4zq+2rxEOUTObxA=";
-          };
-          globalConfig = ''
-            acme_dns cloudflare {env.CF_API_TOKEN}
-          '';
-        };
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
 
-        sops.templates."caddy-env" = {
-          restartUnits = [ "caddy.service" ];
-          content = ''
-            CF_API_TOKEN=${config.sops.placeholder."cloudflare/api_token"}
-          '';
-        };
+  services.caddy = {
+    enable = true;
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-J0HWjCPoOoARAxDpG2bS9c0x5Wv4Q23qWZbTjd8nW84=";
+    };
+    globalConfig = ''
+      acme_dns cloudflare {env.CF_API_TOKEN}
+    '';
+  };
 
-        systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates."caddy-env".path;
+  sops.templates."caddy-env" = {
+    restartUnits = [ "caddy.service" ];
+    content = ''
+      CF_API_TOKEN=${config.sops.placeholder."cloudflare/api_token"}
+    '';
+  };
 
-        networking.firewall.allowedTCPPorts = [
-          80
-          443
-        ];
-      
-    }
+  systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.templates."caddy-env".path;
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+}
