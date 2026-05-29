d870aa53b3e664a26a28e59366428f4b258612eb
trond <trond@nordheim.io>
2026-05-29 13:57:58 +0200

    186         2026-05-29 13:56:16  26.05.20260523.64c08a7    7.0.10   Unknown                 []              True

--- modules/common/claude/default.nix
+++ modules/common/claude/default.nix
@@ -114,9 +114,9 @@ in
 
         onePasswordAgentSocket =
           if isDarwin then
-            "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+            "${config.home.homeDirectory}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
           else
-            "~/.1password/agent.sock";
+            "${config.home.homeDirectory}/.1password/agent.sock";
 
         baseSettings = {
           effortLevel = "xhigh";
