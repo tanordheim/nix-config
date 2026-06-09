22d65d99db021dafea52b67ed94b4b2d418fc8f6
trond <trond@nordheim.io>
2026-06-09 15:53:17 +0200

    197         2026-06-09 15:53:05  26.11.20260606.a799d3e  7.0.11  Unknown                 []              True

--- modules/common/ghostty/_linux.nix
+++ modules/common/ghostty/_linux.nix
@@ -4,10 +4,6 @@
       programs.ghostty.settings = {
         alpha-blending = "native";
         gtk-tabs-location = "hidden";
-        keybind = [
-          "performable:alt+c=copy_to_clipboard"
-          "alt+v=paste_from_clipboard"
-        ];
       };
     }
   ];
