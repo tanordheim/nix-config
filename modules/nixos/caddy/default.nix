94ef03560b8723dfe434c330c54764a696f79453
Probe <probe@example.com>
2026-06-29 22:23:00 +0200

    update caddy hash

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -10,7 +10,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-8yZDrejNKsaUnUaTUFYbarWNmxafqp2z2rWo+XRsxV8=";
+      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
     };
     globalConfig = ''
       acme_dns cloudflare {env.CF_API_TOKEN}
