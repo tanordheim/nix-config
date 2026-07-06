91c671fa509882e7d0bb8b405a6157724fd09830
Probe <probe@example.com>
2026-07-06 20:13:37 +0200

    304         2026-07-06 20:13:31  26.11.20260705.d407951  7.1.3   Unknown                 []              True

--- modules/common/direnv/default.nix
+++ modules/common/direnv/default.nix
@@ -1,13 +1,20 @@
+{ isDarwin, ... }:
 {
   home-manager.sharedModules = [
-    {
-      programs.direnv = {
-        enable = true;
-        enableFishIntegration = false;
-        enableNushellIntegration = false;
-        enableZshIntegration = true;
-        nix-direnv.enable = true;
-      };
-    }
+    (
+      { config, ... }:
+      {
+        programs.direnv = {
+          enable = true;
+          enableFishIntegration = false;
+          enableNushellIntegration = false;
+          enableZshIntegration = true;
+          nix-direnv.enable = true;
+          config.whitelist.prefix = [
+            "${config.home.homeDirectory}/${if isDarwin then "Code" else "code"}/"
+          ];
+        };
+      }
+    )
   ];
 }
