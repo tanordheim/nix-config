5ee99d9df4515929ec94f232c50612eeb20e1a6f
trond <trond@nordheim.io>
2026-08-13 09:56:53 +0200

    62          2026-08-13 09:52:10  26.11.20260809.279b4a8  6.18.43  Unknown                 []              True

--- modules/nixos/caddy/default.nix
+++ modules/nixos/caddy/default.nix
@@ -10,7 +10,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-      hash = "sha256-hEHgAG0F0ozHRAPuxEqLyTATBrE+pajeXDiSNwniorg=";
+      hash = "sha256-7GoH8YLCoPmPExQxoga2FHB58zQDoZVf1BBwkVi0SsQ=";
     };
     globalConfig = ''
       acme_dns cloudflare {env.CF_API_TOKEN}
