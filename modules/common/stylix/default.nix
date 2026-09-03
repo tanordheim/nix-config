ebfddc81550ac7618603fafa6c3b3c6102d524ce
Probe <probe@example.com>
2026-09-03 11:32:26 +0200

    409         2026-09-03 11:25:24  26.11.20260902.3ed67ec  7.2.2   Unknown                 []              True

--- modules/common/stylix/default.nix
+++ modules/common/stylix/default.nix
@@ -25,8 +25,8 @@
         name = "Adwaita Sans";
       };
       monospace = {
-        package = pkgs.nerd-fonts.jetbrains-mono;
-        name = "JetBrainsMono Nerd Font Mono";
+        package = pkgs.aporetic-bin;
+        name = "Aporetic Sans Mono";
       };
       emoji = {
         package = pkgs.noto-fonts-color-emoji;
